1d335ebf5d9114d741d5acac95c968d49ab5cce8
"niutao " <w_niutao@kingsoft.com>
2024-09-11 15:03:29 +0800

    auto_coder_pre_116_chat_action.yml_5d3da23a31d4ffd37c875e13f00218b6

--- my_test/main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# 创建一个文本输入框
-user_input = st.text_input("请输入一些文本：", "")
-
-# 创建一个按钮
-if st.button("显示输入"):
-    # 当按钮被点击时，显示用户输入的内容
-    st.write("你输入了：", user_input)
--- my_test/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-from test1 import fetch_douban_top250  # 导入 fetch_douban_top250 方法
-
-if __name__ == '__main__':
-    fetch_douban_top250()
-
